ed49f52ae08111fbf6549a479296dd27e68bd308
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-03-24 11:03:13 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,14 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Olakkur" }, "geometry": { "type": "Point", "coordinates": [ 79.716892426722836, 12.311814200685699, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Pathiri" }, "geometry": { "type": "Point", "coordinates": [ 79.762306962311442, 12.31745749408236, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Marakkanam" }, "geometry": { "type": "Point", "coordinates": [ 79.94767694839247, 12.196938623277809, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Koonimedu" }, "geometry": { "type": "Point", "coordinates": [ 79.89510701910902, 12.08874728426434, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kiliyanur" }, "geometry": { "type": "Point", "coordinates": [ 79.746772854742304, 12.10721338831326, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thailapuram" }, "geometry": { "type": "Point", "coordinates": [ 79.765545090868201, 12.086873000221249, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Omipper" }, "geometry": { "type": "Point", "coordinates": [ 79.854799275682652, 12.168828742783811, -999999.0 ] } }
+]
+}
---
+++ data/Vulnerable_Villages_6.js
@@ -0,0 +1,23 @@
+var json_Vulnerable_Villages_6 = {
+"type": "FeatureCollection",
+"name": "Vulnerable_Villages_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "village": "Marakkanam (TP)", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.988973290614751, 12.226343919614147 ] } },
+{ "type": "Feature", "properties": { "village": "Marakkanam (TP)", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.951628829321521, 12.199394986810857 ] } },
+{ "type": "Feature", "properties": { "village": "Marakkanam (TP)", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.943493356636736, 12.194987664563968 ] } },
+{ "type": "Feature", "properties": { "village": "Anumandai", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.903169710227587, 12.145986619496442 ] } },
+{ "type": "Feature", "properties": { "village": "Nadukuppam", "Taluk": "Tindivanam", "Type": "Very high" }, "geometry": { "type": "Point", "coordinates": [ 79.885307842705046, 12.158582694166002 ] } },
+{ "type": "Feature", "properties": { "village": "Anumandai", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.910590049086593, 12.131359178009632 ] } },
+{ "type": "Feature", "properties": { "village": "Seyyankuppam", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.897626122917259, 12.130891976320806 ] } },
+{ "type": "Feature", "properties": { "village": "Koonimedu", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.894030969163637, 12.089014258578251 ] } },
+{ "type": "Feature", "properties": { "village": "Kilpudupatti", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.881984478032891, 12.071669191021892 ] } },
+{ "type": "Feature", "properties": { "village": "Kilpudupatti", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.890991517359481, 12.077455798933466 ] } },
+{ "type": "Feature", "properties": { "village": "Kilpudupatti", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.874770434221972, 12.061196908124014 ] } },
+{ "type": "Feature", "properties": { "village": "Kilpudupatti", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.869719667330983, 12.051971975244209 ] } },
+{ "type": "Feature", "properties": { "village": "Kilpudupatti", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.852080305856774, 12.058852949116371 ] } },
+{ "type": "Feature", "properties": { "village": "Olakkur Melpadi", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.716689531235488, 12.311939823483783 ] } },
+{ "type": "Feature", "properties": { "village": "Koochikolathur", "Taluk": "Madurantakam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.748322087346835, 12.315332363696777 ] } },
+{ "type": "Feature", "properties": { "village": "Padiri", "Taluk": "Tindivanam", "Type": "High" }, "geometry": { "type": "Point", "coordinates": [ 79.762793404068731, 12.317649564605734 ] } }
+]
+}
